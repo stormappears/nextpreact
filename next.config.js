92ebc530b04765e10d7ec0534f38fe5b6948d3e4
unknown <nalayakgram@gmail.com>
2023-08-09 11:35:04 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+  webpack: (config, { dev, isServer }) => {
+    // Note, preact is only enabled for production builds (`next build`)
+    if (!dev && !isServer) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        "react/jsx-runtime.js": "preact/compat/jsx-runtime",
+        react: "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat",
+      };
+    }
+ 
+    return config;
+  },
+};
